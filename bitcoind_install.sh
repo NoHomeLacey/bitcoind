c75eb4919fb97b4f4990e682ab83b35a801665b1
NoHomeLacey <37221916+NoHomeLacey@users.noreply.github.com>
2018-03-17 16:17:25 +0000

    Update bitcoind_install.sh
    Tidied a couple of things up - you don't need apt-get upgrade as well as apt-get dist-upgrade.
    
    Also... I've removed all of the sudos - and run the whole thing as root - there's too many restricted commands to run as anything else! I've added an if at the beginning of the script to check if you are root and to exit if not.

--- bitcoind_install.sh
+++ bitcoind_install.sh
@@ -8,6 +8,16 @@
 #########
 
 ########
+# Check script is being run as root
+########
+
+#!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+########
 # Set Hostname
 ########
 
@@ -26,8 +36,8 @@ echo "Enter new hostname: "
 read NEWHOST
 
 #change hostname in /etc/hosts & /etc/hostname
-sudo sed -i "s/$HOSTNAME/$NEWHOST/g" /etc/hosts
-sudo sed -i "s/$HOSTNAME/$NEWHOST/g" /etc/hostname
+sed -i "s/$HOSTNAME/$NEWHOST/g" /etc/hosts
+sed -i "s/$HOSTNAME/$NEWHOST/g" /etc/hostname
 
 #display new hostname
 echo "Your new hostname is $NEWHOST"
@@ -36,57 +46,58 @@ echo "Your new hostname is $NEWHOST"
 # Update repos and install updates
 ########
 
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get dist-upgrade
+apt-get update && apt-get -y dist-upgrade && apt-get clean
 
 #Install dev tools
 
-sudo apt-get install -y autoconf automake build-essential git libtool libgmp-dev libsqlite3-dev python python3 net-tools libsodium-dev
-
+apt-get install -y autoconf automake build-essential git libtool libgmp-dev libsqlite3-dev python python3 net-tools libsodium-dev
 
 #Install additional dependencies for development and testing
 
-sudo apt-get install -y asciidoc valgrind python3-pip
-sudo pip3 install python-bitcoinlib
+apt-get install -y asciidoc valgrind python3-pip
+pip3 install python-bitcoinlib
 
 ########
 #Configure Uncomplicated Firewall to allow access to ssh, bitcoind and lightningd
 ########
 
-sudo ufw enable
-sudo ufw allow 22
-sudo ufw allow 8333
-sudo ufw allow 9735
-
+ufw enable
+ufw allow 22
+ufw allow 8333
+ufw allow 9735
 
 ########
-#Harden ssh server
+# Harden ssh server
 ########
 
 #Install fail2ban which blocks repaeated login attempts (banned for 10 mins after 5 failed logins)
-sudo apt-get install fail2ban
+apt-get -y install fail2ban
 
 #Configure keys based authentication
+cd ~/
 ssh-keygen -t rsa
 chmod 700 ~/.ssh
 chmod 600 ~/.ssh/id_rsa
-cat id_rsa.pub >> ~/.ssh/authorized_keys
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 chmod 700 ~/.ssh
 chmod 600 ~/.ssh/authorized_keys
 
 #Disable root logins
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config.ORIG
-sudo sed -i "s/#PermitRootLogin/PermitRootLogin/g" /etc/ssh/sshd_config
-sudo sed -i "s/PermitRootLogin yes/PermitRootLogin no/g" /etc/ssh/sshd_config
+sed -i "s/#PermitRootLogin/PermitRootLogin/g" /etc/ssh/sshd_config
+sed -i "s/PermitRootLogin yes/PermitRootLogin no/g" /etc/ssh/sshd_config
 
 #Disbale Protocol 1
-sudo sed -i "s/# Protocol 2,1/Protocol 2/g" /etc/ssh/sshd_config
+sed -i "s/# Protocol 2,1/Protocol 2/g" /etc/ssh/sshd_config
 
 #Disable password authentication forcing use of keys
-sudo sed -i "s/PasswordAuthentication yes/PasswordAuthentication no/g" /etc/ssh/sshd_config
-PasswordAuthentication no
+sed -i "s/PasswordAuthentication yes/PasswordAuthentication no/g" /etc/ssh/sshd_config
 
+#PasswordAuthentication no
+
+#Restart Networking to enforce changes
+#Don't worry - it *shouldn't* terminate your current session.
+/etc/init.d/networking restart
 
 ########
 #Install Bitcoin and Lightning
@@ -94,10 +105,10 @@ PasswordAuthentication no
 
 #Install Bitcoind
 
-sudo apt-get install software-properties-common
-sudo add-apt-repository ppa:bitcoin/bitcoin
-sudo apt-get update
-sudo apt-get install -y bitcoind
+apt-get install -y software-properties-common
+add-apt-repository ppa:bitcoin/bitcoin
+apt-get update
+apt-get install -y bitcoind
 
 
 #Install Lightning
@@ -105,7 +116,6 @@ sudo apt-get install -y bitcoind
 git clone https://github.com/ElementsProject/lightning.git
 cd lightning
 make
-
 
 #Run bitcoind and lightningd
 
